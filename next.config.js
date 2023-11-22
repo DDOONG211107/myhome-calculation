ee52f5901e8d7d95d2a87af7d240286f7fb56456
KimYoungSun <rladudtjs154@naver.com>
2023-11-22 20:27:36 +0900

    generateBuildId 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  generateBuildId: async () => {
+    // This could be anything, using the latest git hash
+    return process.env.GIT_HASH;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
