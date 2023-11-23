7ee045409096a399a3cc0c745e71168d84f2bb3d
KimYoungSun <rladudtjs154@naver.com>
2023-11-23 14:09:38 +0900

    버그 수정

--- components/comments/CommentsList.js
+++ components/comments/CommentsList.js
@@ -1,19 +1,23 @@
 export default function CommentsList(props) {
-  return (
-    <ul>
-      {props.comments.map((comment) => {
-        return (
-          <li key={comment.id}>
-            <label>{comment.title} </label>
-            <label>{comment.writer}</label>
-            <br />
-            {comment.comment}
-            <br />
-            <br />
-            <br />
-          </li>
-        );
-      })}
-    </ul>
-  );
+  if (props.comments) {
+    return (
+      <ul>
+        {props.comments.map((comment) => {
+          return (
+            <li key={comment.id}>
+              <label>{comment.title} </label>
+              <label>{comment.writer}</label>
+              <br />
+              {comment.comment}
+              <br />
+              <br />
+              <br />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  } else {
+    return;
+  }
 }
