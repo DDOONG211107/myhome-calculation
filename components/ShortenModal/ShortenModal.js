5a1f882573d631e18e27a6819e5fccf32520b1c1
KimYoungSun <rladudtjs154@naver.com>
2023-12-06 14:00:24 +0900

    모달 css 수정

--- components/ShortenModal/ShortenModal.js
+++ components/ShortenModal/ShortenModal.js
@@ -93,92 +93,101 @@ export default function ShortenModal(props) {
 
   return (
     <div className={classes.shortenModalMain}>
-      <div className={classes.shortenCheck}>
-        <div className={classes.accShorten}>
-          <AccShorten
-            name="firstAcc"
-            title="악세1 생단: "
-            onChange={handleClickFirstAccRadio}
-          />
-          <AccShorten
-            name="secondAcc"
-            title="악세2 생단: "
-            onChange={handleClickSecondAccRadio}
-          />
-          <AccShorten
-            name="thirdAcc"
-            title="악세3 생단: "
-            onChange={handleClickThirdAccRadio}
-          />
-          <AccShorten
-            name="fourthAcc"
-            title="악세4 생단: "
-            onChange={handleClickFourthAccRadio}
-          />
-        </div>
-        <div className={classes.otherShorten}>
-          <HairShorten
-            name="hair"
-            title="머리 생단: "
-            onChange={handleClickHairRadio}
-          />
-          {/* <DressShorten
+      <div className={classes.shortenModalBackground}>
+        <div className={classes.shortenCheck}>
+          <div className={classes.accShorten}>
+            <AccShorten
+              name="firstAcc"
+              title="악세1 생단: "
+              onChange={handleClickFirstAccRadio}
+            />
+            <AccShorten
+              name="secondAcc"
+              title="악세2 생단: "
+              onChange={handleClickSecondAccRadio}
+            />
+            <AccShorten
+              name="thirdAcc"
+              title="악세3 생단: "
+              onChange={handleClickThirdAccRadio}
+            />
+            <AccShorten
+              name="fourthAcc"
+              title="악세4 생단: "
+              onChange={handleClickFourthAccRadio}
+            />
+          </div>
+          <div className={classes.otherShorten}>
+            <HairShorten
+              name="hair"
+              title="머리 생단: "
+              onChange={handleClickHairRadio}
+            />
+            {/* <DressShorten
           title="의상 생단: "
           value={dressShorten}
           onChange={handleClickDressSelect}
         /> */}
-          <div>
-            <label>
-              의상 생단:&nbsp;
-              <select
-                value={dressShorten}
-                onChange={(e) => setDressShorten(e.target.value)}
-              >
-                <option value="0">0%</option>
-                <option value="3">3%</option>
-                <option value="4">4%</option>
-                <option value="5">5%</option>
-                <option value="6">6%</option>
-              </select>
-            </label>
-          </div>
-          <div>
-            <label>
-              펫 생단:&nbsp;
-              <select
-                value={petShorten}
-                onChange={(e) => setPetShorten(e.target.value)}
-              >
-                <option value="0">0%</option>
-                <option value="20">20%</option>
-                <option value="22">22%</option>
-                <option value="25">25%</option>
-                <option value="27">27%</option>
-                <option value="30">30%</option>
-              </select>
-            </label>
-          </div>
-          <div className={classes.portionCheckbox}>
-            <MyCheckBox value={portion1Shorten} setFunc={setPortion1Shorten} />
-            <label>생단 포션1</label>
-            <br />
-            <MyCheckBox value={portion2Shorten} setFunc={setPortion2Shorten} />
-            <label>생단 포션2</label>
+            <div>
+              <label>
+                의상 생단:&nbsp;
+                <select
+                  value={dressShorten}
+                  onChange={(e) => setDressShorten(e.target.value)}
+                >
+                  <option value="0">0%</option>
+                  <option value="3">3%</option>
+                  <option value="4">4%</option>
+                  <option value="5">5%</option>
+                  <option value="6">6%</option>
+                </select>
+              </label>
+            </div>
+            <div>
+              <label>
+                펫 생단:&nbsp;
+                <select
+                  value={petShorten}
+                  onChange={(e) => setPetShorten(e.target.value)}
+                >
+                  <option value="0">0%</option>
+                  <option value="20">20%</option>
+                  <option value="22">22%</option>
+                  <option value="25">25%</option>
+                  <option value="27">27%</option>
+                  <option value="30">30%</option>
+                </select>
+              </label>
+            </div>
+            <div className={classes.portionCheckbox}>
+              <MyCheckBox
+                value={portion1Shorten}
+                setFunc={setPortion1Shorten}
+              />
+              <label>생단 포션1</label>
+              <br />
+              <MyCheckBox
+                value={portion2Shorten}
+                setFunc={setPortion2Shorten}
+              />
+              <label>생단 포션2</label>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={classes.shortenResult}>
-        총 생산단축&nbsp;
-        {totalShorten}%
+        <div className={classes.shortenResult}>
+          총 생산단축:
+          {totalShorten}%
+          <div className={classes.buttonDiv}>
+            <div onClick={clickApplyHandler} className={classes.button}>
+              적용하기
+            </div>
+            <div onClick={props.onClose} className={classes.cancelButton}>
+              취소
+            </div>
+          </div>
+          <br />
+        </div>
         <br />
-        <div className={classes.buttonDiv}>
-          <div onClick={clickApplyHandler} className={classes.button}>
-            적용하기
-          </div>
-          <div onClick={props.onClose} className={classes.cancelButton}>
-            취소
-          </div>
-        </div>
       </div>
     </div>
   );
